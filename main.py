0d3303fca9c1edd4c59fcf8012f08cd686f821e3
Wilson Icheku <ichekuwilson538@gmail.com>
2024-07-02 09:58:24 +0100

    redeploy

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ def root():
 
 
 @app.get('/api/hello')
-async def SayHello(visitors_name: str = None,):
+async def SayHello(request: Request, visitors_name: str = None):
     if not visitors_name:
         return {"message": "Hello Dear, welcome to my application."}
     get_location_url = f'{setting.api_url}?token={setting.token}'
+    client_ip = request.client.host
+    print(client_ip)
     location = requests.get(get_location_url).json()
     get_temp_url = f'{setting.temp_url}?q={location["city"]}&appid={setting.api_key}'
 
@@ -28,7 +30,7 @@ async def SayHello(visitors_name: str = None,):
     temperature = temp_info['main']['temp']
 
     return {
-        "client_ip": f'{location["ip"]}',
+        "client_ip": client_ip,
         'location': location['city'],
         "message": f"hello {visitors_name} the temperature is {temperature} degrees in {location['city']}",
 
